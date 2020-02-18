a60b0e592376ea7acfc6e07d3f50f73825251e07
Morgan Stone <morgan.stone@healthgrades.com>
2020-02-18 12:48:50 -0600

    move to _static. Add html min

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,21 @@
+const htmlmin = require('html-minifier');
+const CleanCSS = require('clean-css');
+
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ 'static': '.' });
+  eleventyConfig.addFilter('cssmin', (code) => {
+    return new CleanCSS({}).minify(code).styles;
+  });
+  eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+    if( !outputPath.endsWith('.html') ) {
+      return content;
+    }
+    return htmlmin.minify(content, {
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true
+    });
+  });
+  eleventyConfig.addPassthroughCopy({ '_static': '.' });
   return {
     passthroughFileCopy: true,
     templateFormats: [
