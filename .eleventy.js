8ecc671796232c8642464d3a05b4e36b465a0625
Morgan Stone <mstone@designbymorgan.com>
2021-08-01 22:49:45 -0500

    Add BBQ Links to sub site

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,38 @@
 const htmlmin = require('html-minifier');
 const CleanCSS = require('clean-css');
 const Terser = require('terser');
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, className = "", widths = "300,600", sizes = "100vw") {
+  if(alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+  }
+  console.log(widths.split(','))
+  let metadata = await Image(src, {
+    widths: widths.split(','),
+    formats: ['webp', 'jpeg'],
+    outputDir: './dist/images/',
+    urlPath: '/images/'
+  });
+
+  let lowsrc = metadata.jpeg[0];
+  let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+
+  return `<picture class="${className}">
+    ${Object.values(metadata).map(imageFormat => {
+      return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
+    }).join("\n")}
+      <img
+        src="${lowsrc.url}"
+        width="${highsrc.width}"
+        height="${highsrc.height}"
+        alt="${alt}"
+        loading="lazy"
+        decoding="async">
+    </picture>`;
+}
+
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter('cssmin', (code) => new CleanCSS({}).minify(code).styles);
@@ -23,6 +55,10 @@ module.exports = function(eleventyConfig) {
       collapseWhitespace: true
     });
   });
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
 
   eleventyConfig.addPassthroughCopy({ 'src/_static': '.' });
   return {
--- src/_includes/ga.js
+++ src/_includes/ga.js
@@ -3,4 +3,17 @@ ga.q = [];
 ga.l = +new Date;
 ga('create', 'UA-1337770-1', 'auto');
 ga('set','transport','beacon');
-ga('send', 'pageview')
+ga('send', 'pageview');
+var captureOutboundLink = function(url) {
+  ga('send', 'event', 'outbound', 'click', url, {
+    'transport': 'beacon',
+    'hitCallback': function(){document.location = url;}
+  });
+}
+
+var captureSocialOutboundLink = function(social, url) {
+  ga('send', 'event', 'social', 'click', social, {
+    'transport': 'beacon',
+    'hitCallback': function(){document.location = url;}
+  });
+}
